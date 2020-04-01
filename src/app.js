c11aa5ca49a836349323e150006ffc453a27fdb6
ittyBittyBytes <loralie.flint0128@gmail.com>
2020-03-31 21:05:53 -0700

    trying to deploy to zeit again

--- src/app.js
+++ src/app.js
@@ -1,29 +1,26 @@
 import React from "react";
 import "./App.css";
-import { Route, Link } from "react-router-dom";
-import Nav from "./nav"
-import Home from "./home"
-import Quotes from "./quotes"
-import Smile from "./smile"
+import { Route } from "react-router-dom";
+import Nav from "./nav";
+import Home from "./home";
+import Quotes from "./quotes";
+import Smile from "./smile";
 function App() {
-
   return (
     <div className="app">
-      <Nav/>
-       <Route exact path="/">
-        <Home/>
-       </Route>
+      <Nav />
+      <Route exact path="/">
+        <Home />
+      </Route>
 
-       <Route path="/smile">
-         <Smile/>
+      <Route path="/smile">
+        <Smile />
       </Route>
 
       <Route path="/quotes">
-         <Quotes/>
+        <Quotes />
       </Route>
-
     </div>
   );
-
-  }
+}
 export default App;
